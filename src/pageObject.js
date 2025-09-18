4d8e58eb0398151eb3232d207a76af96ce6442e0
Adam <adamur.ichigo@onet.pl>
2025-09-18 14:49:24 +0200

    Fix ESLint errors and export PageObject classes

--- src/pageObject.js
+++ src/pageObject.js
@@ -1,19 +1,41 @@
-'use strict';
-
+// Header class
 class Header {
+  get logo() {
+    return 'Conduit logo';
+  }
+}
 
-};
-
+// PageObject class
 class PageObject {
+  constructor(url) {
+    this.url = url;
+    this.header = new Header();
+  }
 
-};
+  clickOnLogo() {
+    return 'Click on the ' + this.header.logo;
+  }
+}
 
+// ArticlePage class
 class ArticlePage extends PageObject {
+  constructor(url, slug) {
+    super(url);
+    this.url = `${url}${slug}`;
+  }
 
-};
+  get commentButton() {
+    return '[Publish comment] button';
+  }
 
-module.exports = {
-  Header,
-  PageObject,
-  ArticlePage,
-};
+  clickOnCommentButton() {
+    return 'Click on the ' + this.commentButton;
+  }
+
+  assertPageOpened() {
+    return `The ${this.url} is opened`;
+  }
+}
+
+// Eksport klas
+module.exports = { Header, PageObject, ArticlePage };
